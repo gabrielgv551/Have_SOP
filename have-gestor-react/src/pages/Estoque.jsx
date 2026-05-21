3095e60be5a12a02466bc12bb5bde38c5b004d20
Gabriel <gabrielgv551@users.noreply.github.com>
2026-05-21 17:20:11 -0300

    chore: remove abandoned have-gestor-react migration prototype

--- have-gestor-react/src/pages/Estoque.jsx
+++
@@ -1,8 +0,0 @@
-export default function Estoque() {
-  return (
-    <div className="p-6">
-      <h1 className="text-xl font-bold text-[#1c2d4a]">Estoque</h1>
-      <p className="text-sm text-gray-400 mt-1">Em migração...</p>
-    </div>
-  )
-}
